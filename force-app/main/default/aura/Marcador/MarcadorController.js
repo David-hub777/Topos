2bebd496fa0633b10391450c9a9969e31e5c8e45
David <david25042001@gmail.com>
2024-02-05 10:42:51 +0100

    Marcador y creacion Tabla de Puntuaciones

--- force-app/main/default/aura/Marcador/MarcadorController.js
+++ force-app/main/default/aura/Marcador/MarcadorController.js
@@ -11,5 +11,22 @@
             marcador += pointsToAdd;
         }
         component.set('v.marcador', marcador);// Es obligatorio volver a mandar la variable marcador a la vista
+    },
+    guardarScore : function(component, event, helper) {
+        var insertScoreApex = component.get("c.insertScore");
+
+        insertScoreApex.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                console.log("Score guardado");
+            }else{
+                console.log("Error: " + state);
+            }
+        });
+        insertScoreApex.setParams({
+            "nombre": "marcador",
+            "puntuacion": component.get("v.marcador")
+        });
+        $A.enqueueAction(insertScoreApex);
     }
 })
